837896d275e927e3046ecd1712f8638e747f4803
Saurabh Singh <saurabh6375singh@gmail.com>
2022-01-16 20:37:56 +0530

    Create truffle-config.js

---
+++ truffle-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  networks: {
+    vertigo_test_network_1: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+    vertigo_test_network_2: {
+      host: "127.0.0.1",
+      port: 8546,
+      network_id: "*"
+    },
+  }
+}
